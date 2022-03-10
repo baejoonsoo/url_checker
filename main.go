d2c352cab74fd73675bc3892316d5ac71bf02f9a
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-10 22:51:16 +0900

    http.Get

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"net/http"
+)
+
+var errRequestFailed=errors.New("request failed")
 
 func main(){
-	fmt.Println("url_checker")
+	urls := []string{
+		"https://www.aribnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _,url :=range urls{
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error{
+	res, err:= http.Get(url)
+
+	if err==nil|| res.StatusCode>=400{
+		return errRequestFailed
+	}
+	return nil
 }
