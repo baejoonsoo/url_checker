7faf6b1d5bb604e7086386c6b5e604aa5e4c7c65
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-12 03:29:17 +0900

    Feat: Receive and output values ​​through channel

--- main.go
+++ main.go
@@ -5,16 +5,15 @@ import (
 	"net/http"
 )
 
-// var errRequestFailed=errors.New("request failed")
 
-type result struct {
+type requestResult struct {
 	url string
 	status string
 }
 
 func main(){
-	channel :=make(chan result)
-	// results := map[string]string{}
+	channel :=make(chan requestResult)
+	results := map[string]string{}
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -30,10 +29,19 @@ func main(){
 	for _,url :=range urls{
 		go hitURL(url, channel)
 	}
+
+	for i := 0; i<len(urls);i++{
+		result := <-channel
+
+		results[result.url]=result.status
+	}
+
+	for url, status := range results{
+		fmt.Println(url, status)
+	}
 }
 
-func hitURL(url string, channel chan<- result) {
-	fmt.Println("Checking",url)
+func hitURL(url string, channel chan<- requestResult) {
 	res, err:= http.Get(url)
 
 	status := "OK"
@@ -41,7 +49,7 @@ func hitURL(url string, channel chan<- result) {
 	if err!=nil || res.StatusCode>=400{
 		status = "FAILED"
 	}
-	channel<-result{url:url,status: status}
+	channel<-requestResult{url:url,status: status}
 
 	
 }
